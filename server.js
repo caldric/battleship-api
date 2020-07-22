2425cd98929855e586cfb070b9dbc2a6e1597930
caldric <cvaldric@gmail.com>
2020-07-22 00:40:07 -0400

    Test change in client Heroku URL

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const local = {
 const deployment = {
   port: process.env.PORT,
   mongoURI: process.env.MONGODB_URI,
-  url: 'https://salty-brook-60152.herokuapp.com',
+  url: 'https://separate-client.herokuapp.com',
 };
 const index = 'index';
 const app = express();
